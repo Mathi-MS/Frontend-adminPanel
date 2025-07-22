80b2ab9df95905af485efce5a7c891ace85a5b55
Mathiazhaganhepl <mathiazhagan.s@hepl.com>
2025-07-22 22:06:40 +0530

    First Commit

--- src/assets/Validation/Schema.tsx
+++ src/assets/Validation/Schema.tsx
@@ -9,7 +9,7 @@ export const LoginSchema = z.object({
   password: z
     .string()
     .min(1, { message: "Password is required" })
-    .min(8, { message: "Password must be at least 8 characters" })
+    // .min(8, { message: "Password must be at least 8 characters" })
     .max(12, { message: "Password must be not more than 12 characters" }),
 });
 
